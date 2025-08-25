9aea2f10965a067413c984b66ec85d3bbec6b928
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-08-25 13:39:36 +0200

    Exercise/POOCours first commit

---
+++ POO-Course/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace POO_Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
